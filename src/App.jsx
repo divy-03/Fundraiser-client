4c275f137568339979898813b7dd6400a69ad8b6
divy-03 <127951881+divy-03@users.noreply.github.com>
2025-03-20 21:49:35 +0530

    added imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
+import { useEffect } from "react";
 import "./styles/app.scss";
+import { useGetUserProfileQuery } from "./apis/userApi";
+import { useDispatch, useSelector } from "react-redux";
+import { userExist, userNotExist } from "./reducers/userReducer";
 
 const App = () => {
+  const { user, loading } = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const { data, isLoading, error } = useGetUserProfileQuery();
+
+  useEffect(() => {
+    if (data) {
+      dispatch(userExist(data.user));
+    } else if (error) {
+      dispatch(userNotExist());
+    }
+  }, [data, error, dispatch]);
+
+  if (loading || isLoading) return <div>Loading...</div>;
   return (
     <>
       <h1>App</h1>
